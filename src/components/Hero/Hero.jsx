436a42cabaeb9697845f171f6dc67edc46ab1c74
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2024-04-02 19:26:05 +0300

    ➕added  section hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,68 @@
+import { useSelector } from "react-redux";
+import bookRoomImage from "../../assets/homePageImgs/HeroImg.jpg";
+import bookRoomImageDesk from "../../assets/homePageImgs/HeroImgDesk.jpg";
+import "./Hero.scss";
+
+export default function Hero() {
+  const isMobile = useSelector(store => store.app.isMobile);
+  return (
+    <section className="hero">
+      <h1 className="font-mono italic text-[#FCD043] text-[62px]">
+        <span className="text-[#E5E5E5]">Bank</span>Hotel
+      </h1>
+      <div className="hero__inner-wrapper">
+        <p className="font-serif text-center	text-[#FCD043] text-sm">
+          rooms // restaurant // congress hall // wine bar
+        </p>
+        <p
+          className={`hero__description ${
+            isMobile && "hero__description--under mt-[24px] mb-[40px]"
+          }  max-w-[290px] font-mono text-[#E5E5E5] ${
+            isMobile && "text-center"
+          } text-xs`}
+        >
+          The luxurious hotel in the most beautiful European city with an
+          exclusive restaurant, conference-hall, and art-bar.
+        </p>
+      </div>
+      <div className="relative">
+        <img
+          className="mb-[50px]"
+          src={isMobile ? bookRoomImage : bookRoomImageDesk}
+          alt="the room with a table"
+        />
+
+        <ul className="hero__actions">
+          <li className={isMobile ? "visually-hidden" : "hero__btn"}>
+            <button type="button">Check in</button>
+          </li>
+          <li className={isMobile ? "visually-hidden" : "hero__btn"}>
+            <button type="button">Check out</button>
+          </li>
+          <li className="hero__btn hero__btn--booking">
+            <button type="button">Book room</button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="hero__inner-wrapper">
+        <div className={isMobile ? "visually-hidden" : "hero__contacts"}>
+          <a className="underline italic" href="phone: +380322975020">
+            +38 032 297 50 20
+          </a>
+          <address>
+            <span>8 Lystopadovoho Chynu, Lviv</span>
+          </address>
+        </div>
+
+        <p
+          className={`max-w-[117px] font-mono text-[#E5E5E5] ${
+            isMobile ? "text-left" : "text-right"
+          }  text-xs uppercase`}
+        >
+          Art & Congress hall
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,14 @@
 import "./Home.scss";
+import aboutImage from "../../assets/homePageImgs/about.jpg";
+import Hero from "../../components/Hero/Hero";
 
 export default function Home() {
   return (
-    <div className="p-[10px] bg-[#313F38] ">
-      <h1 className="font-mono text-[#FCD043] text-[62px]">
-        <span className="text-[#E5E5E5]">Bank</span>Hotel
-      </h1>
-      <p className="text-center	text-[#FCD043] text-sm">
-        rooms // restaurant // congress hall // wine bar
-      </p>
-      <p className="description mt-[24px] max-w-[290px] font-mono text-[#E5E5E5] text-center text-xs">
-        The luxurious hotel in the most beautiful European city with an
-        exclusive restaurant, conference-hall, and art-bar.
-      </p>
+    <div className="font-mono p-[10px] bg-[#313F38] ">
+      <Hero />
+
+      <h2 className="subtitle">About US</h2>
+      <img className="mb-[50px]" src={aboutImage} alt="about image" />
     </div>
   );
 }
